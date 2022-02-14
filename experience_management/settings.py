1e10ca7170d556f73595f869c47f30b96afd4d57
raj <48752322+raj-bunsha@users.noreply.github.com>
2022-02-14 19:04:54 +0530

    Update settings.py

--- experience_management/settings.py
+++ experience_management/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY="SECRCET"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["exp-management.herokuapp.com","127.0.0.1"]
 
 AUTH_USER_MODEL='main.Account'
